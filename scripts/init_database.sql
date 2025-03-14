27bc2339573ca052b2ba25dbd06a69cd5691f046
weikoh <73581236+weikoh@users.noreply.github.com>
2025-03-14 15:14:48 +0200

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,20 @@
+/*
+=============================
+Create Database and Schemas
+=============================
+Script Purpose:
+  Creates new database and drops old if already exists: 'DataWarehouse'
+  Creates three chemas: bronze, silver, gold
+WARNING:
+  It will delete old database!
+*/
+use master;
+CREATE DATABASE DataWarehouse;
+USE DataWarehouse;
 
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
